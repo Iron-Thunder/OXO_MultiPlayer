e326e30433b257128a3d301983a7c7ea3e548ca0
Iron-Thunder <akashranipokhar@gmail.com>
2023-09-12 00:42:01 +0530

    Added create room and join room page and created custom button, text and text_field

--- lib/screens/create_room.dart
+++ lib/screens/create_room.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Responsive/responsive.dart';
+import 'package:flutter_application_1/Widgets/custom_button.dart';
+import 'package:flutter_application_1/Widgets/custom_text.dart';
+import 'package:flutter_application_1/Widgets/custom_textField.dart';
 
 class CreateRoom extends StatefulWidget {
   static String routeName = "/create-room";
@@ -9,9 +13,45 @@ class CreateRoom extends StatefulWidget {
 
 }
 
+
 class _CreateRoomState extends State<CreateRoom> {
+
+  final TextEditingController _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _nameController.dispose();
+  }
+
   @override
   Widget build(BuildContext context){
-    return Container();
+    final Size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Responsive(
+        child: Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [ // we are not using const here for this child bcz we are going to make it responsive and for that we have to change them for different screens
+             const CustomText(shadows: [
+              Shadow(
+                blurRadius: 40,
+                color: Colors.blue,
+              )
+             ], text: "Create Room", fontSize: 70),
+             SizedBox(height: Size.height*0.08,),
+             CustomTextField(controller: _nameController, hintText: 'Enter your Nickname'),
+             SizedBox(height: Size.height*0.04,),
+             CustomButton(onTap: () {}, text: "Create")
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/join_room.dart
+++ lib/screens/join_room.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Responsive/responsive.dart';
+import 'package:flutter_application_1/Widgets/custom_button.dart';
+import 'package:flutter_application_1/Widgets/custom_text.dart';
+import 'package:flutter_application_1/Widgets/custom_textField.dart';
 
 class JoinRoom extends StatefulWidget {
   static String routeName = "/join-room";
@@ -10,8 +14,48 @@ class JoinRoom extends StatefulWidget {
 }
 
 class _JoinRoomState extends State<JoinRoom> {
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _roomController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose : it is done so that the memory space is not exceeded that it it doesnot overflows from the memory
+    super.dispose();
+    _nameController.dispose();
+    _roomController.dispose();
+  }
+
   @override
   Widget build(BuildContext context){
-    return Container();
+    final Size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Responsive(
+        child: Container(
+          margin: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [ // we are not using const here for this child bcz we are going to make it responsive and for that we have to change them for different screens
+             const CustomText(shadows: [
+              Shadow(
+                blurRadius: 40,
+                color: Colors.blue,
+              )
+             ], text: "Join Room", fontSize: 70),
+             SizedBox(height: Size.height*0.08,),
+             CustomTextField(controller: _nameController, hintText: 'Enter your NickName'),
+             SizedBox(height: Size.height*0.03,),
+             CustomTextField(controller: _roomController, hintText: 'Enter your TeamCode'),
+             SizedBox(height: Size.height*0.05,),
+             CustomButton(onTap: () {}, text: "Join")
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
